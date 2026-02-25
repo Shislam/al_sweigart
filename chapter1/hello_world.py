d7bd00bf01b068e7a48d214dc2a482ffecf58a6a
Shislam <shislam21@gmail.com>
2026-02-25 01:06:10 -0600

    added first program from first chapter

---
+++ chapter1/hello_world.py
@@ -0,0 +1,11 @@
+# name input output
+print('Hello World!')
+print('What is your name, friend?')
+name = input()
+print('Hello, ' + name +'!')
+print('The length of your name is: ' + str(len(name)) +'.')
+
+# age
+age = input('How old are you? ')
+print('You are ' + str(age) + ' year(s) old.')
+
